e19b039204469615087a037c9a58ce25ed49dcc4
Sayanaann <alexsayana@gmail.com>
2022-11-25 00:48:56 +0530

    all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { SearchEmployeeComponent } from './search-employee/search-employee.component';
+import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
+import { ViewAllEmployeeComponent } from './view-all-employee/view-all-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent
+    AdminLoginComponent,
+    AddEmployeeComponent,
+    SearchEmployeeComponent,
+    DeleteEmployeeComponent,
+    ViewAllEmployeeComponent,
+    EditEmployeeComponent
   ],
   imports: [
     BrowserModule,
